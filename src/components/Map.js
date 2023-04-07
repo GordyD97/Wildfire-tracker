adb9f4ecffaf7153e4091594a99fb72f5d6ba100
Gordon D <gordon.draine97@yahoo.com>
2023-04-06 22:00:51 -0400

    spelled categories wrong, now the error message stack collapse is gone.

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,10 +4,13 @@ import React from 'react'
 import LocationMarker from './LocationMarker'
 import LocationinfoBox from './LocationinfoBox'
 
+const NATURAL_EVENT_WILDFIRE = 8
+
 const Map = ({ eventData, center, zoom }) => {
   const [locationinfo, setLocationInfo] = useState(null)
-  const markers = eventData.map(ev =>{
-    if(ev.categoiries[0].id ===8 ) {
+
+  const markers = eventData.map((ev, index) => {
+    if (ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
       return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
     }
     return null
